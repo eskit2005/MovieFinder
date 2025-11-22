293b7fabcfdf9d61d3df5e30ed171d7f40858a31
eskit2005 <ibrahim020105@gmail.com>
2025-11-22 20:45:16 +0600

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouterRouter>
+  <HashRouter>
     <StrictMode>
       <App />
     </StrictMode>,
-  </HashRouterRouter>
+  </HashRouter>
   
 )
