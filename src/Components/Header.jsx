be8e4555e88519d27ea1550b90ecc60a238b39e7
eskit2005 <ibrahim020105@gmail.com>
2025-11-23 00:07:08 +0600

    added sidebar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,31 @@
-import { Link } from 'react-router-dom';
-import popcorn from '../assets/popcorn.svg';
-import './Header.css';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import popcorn from "../assets/popcorn.svg";
+import "./Header.css";
+
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-        <>
-            <nav className="header-container">
-                <div className="logo-name">
-                    <img src={popcorn} alt="" />
-                    <h1>Movie Search</h1>
-                </div>
-                <div className="header-links">
-                    <Link to="/"><h1>Home</h1></Link>
-                    <Link to="/Favorites"><h1>Favorites</h1></Link>
-                </div>
-            </nav>
-        </>
-    )
+        <nav className="header-container">
+            <div className="logo-name">
+                <img src={popcorn} alt="" />
+                <h1>Movie Search</h1>
+            </div>
+
+            <button 
+                className="menu-toggle" 
+                onClick={() => setMenuOpen(!menuOpen)}
+            >
+                ☰
+            </button>
+
+            <div className={`header-links ${menuOpen ? "open" : ""}`}>
+                <Link to="/" ><h1>Home</h1></Link>
+                <Link to="/Favorites" onClick={() => setMenuOpen(false)}><h1>Favorites</h1></Link>
+            </div>
+        </nav>
+    );
 }
+
 export default Header;
